a6d55976085c6ae311f436a5e4aca17d40afe604
Gabriele Falace <gabriele.falace@asideas.de>
2017-06-18 00:48:54 +0200

    a status apparently almost working

---
+++ scripts/header.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    var content = '<h1><a href="../index.html">Architektur und Entwicklung</a></h1><nav><ul id="barb"><li><button id="b1b"><a href="http.html">HTTP</a></button></li><li><button id="b2b"><a href="ansible.html">Ansible</a></button></li><li><button id="b3b"><a href="go.html">Go</a></button></li><li><button id="b4b"><a href="spring.html">Spring</a></button></li><li><button id="b5b"><a href="spring_data.html">Spring Data</a></button></li><li><button id="b6b"><a href="tdd.html">TDD</a></button></li><li><button id="b7b"><a href="python.html">Python</a></button></li><li><button id="b8b"><a href="jenkins.html">Jenkins</a></button></li></ul></nav></div>';
+    $('#header').html(content);
+});
