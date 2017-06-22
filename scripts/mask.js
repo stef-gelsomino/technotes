bb0ffac1f833fe0d9744a57f54f7db48b783efd8
Gabriele Falace <gabriele.falace@asideas.de>
2017-06-22 19:06:31 +0200

    single script for footer and header

--- scripts/mask.js
+++ scripts/mask.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
     var currentURL = window.location.href;
-    var elenco = currentURL.split('/');
-    var len = elenco.length;
-    var pageName = elenco[len-1];
+    var elements = currentURL.split('/');
+    var n = elements.length;
+    var pageName = elements[n-1];
 
     var prefixDir = '../';
     var prefixArticle = '';
@@ -12,10 +12,7 @@ $(document).ready(function() {
         prefixArticle = 'articles/';
     }
 
-
-
-
-        var content = '<h1><a href="' + prefixDir + 'index.html">Architektur und Entwicklung</a></h1>' +
+        var headerContent = '<h1><a href="' + prefixDir + 'index.html">Architektur und Entwicklung</a></h1>' +
                       '<nav><ul id="barb"><li><button id="b1b"><a href="'+ prefixArticle +'http.html">HTTP</a></button></li>' +
                                          '<li><button id="b2b"><a href="'+ prefixArticle +'ansible.html">Ansible</a></button></li>' +
                                          '<li><button id="b3b"><a href="'+ prefixArticle +'go.html">Go</a></button></li>' +
@@ -26,5 +23,13 @@ $(document).ready(function() {
                                          '<li><button id="b7b"><a href="'+ prefixArticle +'python.html">Python</a></button></li>' +
                                          '<li><button id="b8b"><a href="'+ prefixArticle +'jenkins.html">Jenkins</a></button></li></ul></nav>';
 
-    $('#header').html(content);
+    $('#header').html(headerContent);
+
+    var footerContent = '<div id="fool"> <h3><a target="_blank" href="https://www.linkedin.com/in/stefano-gelsomino/"><span>Site by</span> <span>Stefano Gelsomino</span> </a></h3>' +
+                        ' </div> <div id="follow"> <a target="_blank" href="https://www.linkedin.com/in/stefano-gelsomino/"> <img src="'+ prefixDir + 'img/linkedin.png"/> </a> </div> ' +
+                        '<div id="foor"> <h3>2017</h3> </div>';
+    $('.footer').html(footerContent);
 });
+
+
+
