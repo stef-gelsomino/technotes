d218d3cd2a5414837d190bfa013d50dd6e88a1c7
Stefano J <Stefano J>
2017-07-08 20:20:18 +0200

    fixed some bugs; added flexible layout

--- scripts/mask.js
+++ scripts/mask.js
@@ -31,5 +31,24 @@ $(document).ready(function() {
     $('.footer').html(footerContent);
 });
 
+/* DROP-DOWN-MENU */
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/***********************************************************************************************/
