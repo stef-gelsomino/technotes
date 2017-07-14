4350b2a35b52f61148e9b32107c0546722e2019e
Gabriele Falace <gabriele.falace@asideas.de>
2017-07-14 17:24:54 +0200

    fixed bug on LI icon

--- scripts/mask.js
+++ scripts/mask.js
@@ -30,9 +30,14 @@ $(document).ready(function () {
 
     $('#header').html(headerContent);
 
+    var linkedinIcon = prefixDir + 'img/linkedin.png';
+
     var footerContent = '<div id="fool"> <h3><a target="_blank" href="https://www.linkedin.com/in/stefano-gelsomino/"><span>Site by</span> <span>Stefano Gelsomino</span> </a></h3>' +
-                        ' </div> <div id="follow"> <a target="_blank" href="https://www.linkedin.com/in/stefano-gelsomino/"> <img src="'+ prefixDir + 'img/linkedin.png"/> </a> </div> ' +
+                        ' </div> <div id="follow"> <a target="_blank" href="https://www.linkedin.com/in/stefano-gelsomino/"> <img src="%placeholder%"/> </a> </div> ' +
                         '<div id="foor"> <h3>2017</h3> </div>';
+
+    footerContent = footerContent.replace('%placeholder%', linkedinIcon);
+
     $('.footer').html(footerContent);
 });
 
@@ -54,6 +59,6 @@ window.onclick = function(event) {
       }
     }
   }
-}
+};
 
 /***********************************************************************************************/
